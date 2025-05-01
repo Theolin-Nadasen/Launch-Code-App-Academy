84cd3ff0e85a7594b2edd0d23a6731e1fe479509
Theolin Nadasen <nadasentheolin@gmail.com>
2025-05-01 13:43:16 +0200

    added the css and js

---
+++ app.js
@@ -0,0 +1,8 @@
+// Get the button element by ID
+const btn = document.getElementById("changeTextBtn");
+
+// Add a click event listener
+btn.addEventListener("click", function () {
+  alert("You clicked the button!");
+  // TODO: Change the text of the button after it's clicked
+});
